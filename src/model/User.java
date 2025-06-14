0542f8b32d9e47e554beea9890c0f6750dfcd277
dyana-vc <ackermandyana@gmail.com>
2025-06-14 15:37:26 -0400

    Actualización: perfil estudiante, AuthService y UI

--- src/model/User.java
+++ src/model/User.java
@@ -1,52 +1,97 @@
 package model;
 
-import java.sql.Date;
-
 public class User {
-    public String id;
-    public String username;
-    public String password;
-    public String role;
-    public String name;
-    public String lastName;
-    public String country;
-    public String email;
-    public Date timeZone; 
-    
-    public User(String id, String username, String password, String role, String name, String lastName, String country, String email, Date timeZone) {
+    private String id;
+    private String username;
+    private String password;
+    private String role;
+
+    // Nuevos campos para perfil editable
+    private String nombre;
+    private String apellido;
+    private String ciudad;
+    private String correo;
+    private String timezone;
+
+    // Constructor
+    public User(String id, String username, String password, String role) {
         this.id = id;
         this.username = username;
         this.password = password;
         this.role = role;
-        this.name = name;
-        this.lastName = lastName;
-        this.country = country;
-        this.email = email;
-        this.timeZone = timeZone;
     }
 
-    public User(String id, String username, String password, String role) {
-    this.id = id;
-    this.username = username;
-    this.password = password;
-    this.role = role;
-   }
-
+    // Getters y setters para id, username, password y role
     public String getId() {
         return id;
     }
 
-    public String getUsername() { return username; }
-    public String getPassword() { return password; }
-    public String getRole() { return role; }
-    public String getNombre() { return name; }
-    public String getEmail() { return email; }
-    
-    public void setNombre(String name) { this.name = name; }
-    public void setEmail(String email) { this.email = email; }
+    public void setId(String id) {
+        this.id = id;
+    }
 
-    @Override
-    public String toString() {
-        return "User{id='" + id + "', username='" + username + "', role='" + role + "'}";
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    // Getters y setters para los nuevos campos
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getTimezone() {
+        return timezone;
+    }
+
+    public void setTimezone(String timezone) {
+        this.timezone = timezone;
     }
 }
